b09ca69032bc5ea741f2fda7baf8c863a7e60dd0
Jarmo Pertman <jarmo.p@gmail.com>
2010-01-12 21:47:51 +0200

    added files for post "We need more to be shown in the reports?!": all files saved during the examples will be shown on reports

--- watir_html_formatter.rb
+++ watir_html_formatter.rb
@@ -11,27 +11,31 @@ class WatirHtmlFormatter < Spec::Runner::Formatter::HtmlFormatter
     @output_dir = File.dirname(output)
     @files_dir = File.join(@output_dir, "files")
     FileUtils.mkdir_p(@files_dir) unless File.exists?(@files_dir)
+    $formatter = self
+    super
+  end
+
+  def example_started(example)
+    @files_saved_during_example = []
     super
   end
 
   def extra_failure_content(failure)
+    save_html
+    save_screenshot
+
     content = []
     content << "<span>"
-    file_name = save_html
-    content << link_for(file_name)
-    
-    file_name = save_screenshot
-    content << link_for(file_name)
-
+    @files_saved_during_example.each {|f| content << link_for(f)}
     content << "</span>"
     super + content.join($/)
   end
 
-  def link_for(file_name)
-    return unless file_name && File.exists?(file_name)
+  def link_for(file)
+    return unless File.exists?(file[:path])
 
-    description = File.extname(file_name).upcase[1..-1]
-    path = Pathname.new(file_name)
+    description = file[:desc] ? file[:desc] : File.extname(file[:path]).upcase[1..-1]
+    path = Pathname.new(file[:path])
     "<a href='#{path.relative_path_from(Pathname.new(@output_dir))}'>#{description}</a>&nbsp;"
   end
 
@@ -47,11 +51,11 @@ class WatirHtmlFormatter < Spec::Runner::Formatter::HtmlFormatter
     file_name
   end
 
-  def save_screenshot
+  def save_screenshot(description="Screenshot")
     begin
       $browser.bring_to_front
       width, height, blob = Win32::Screenshot.capture_hwnd($browser.hwnd)
-      file_name = file_path("screenshot.png")
+      file_name = file_path("screenshot.png", description)
       img = Magick::ImageList.new
       img.from_blob(blob)
       img.write(file_name)
@@ -62,11 +66,20 @@ class WatirHtmlFormatter < Spec::Runner::Formatter::HtmlFormatter
     file_name
   end
   
-  def file_path(file_name)
+  def file_path(file_name, description=nil)
     extension = File.extname(file_name)
     basename = File.basename(file_name, extension)
     file_path = File.join(@files_dir, "#{basename}_#{Time.now.strftime("%H%M%S")}_#{example_group_number}_#{example_number}#{extension}")
+    @files_saved_during_example.unshift(:desc => description, :path => file_path)
     file_path
+  end
+
+  def native_file_path(path)
+    File::ALT_SEPARATOR ? path.gsub(File::SEPARATOR, File::ALT_SEPARATOR) : path
+  end
+
+  def absolute_file_path(file_name, description=nil)
+    native_file_path(File.expand_path(file_path(file_name, description)))
   end
 
 end
--- watir_with_rspec.rb
+++ watir_with_rspec.rb
@@ -17,6 +17,22 @@ describe "Google" do
     $browser.title.should == "Bing"
   end
 
+  it "uses formatter's file_path method and fails" do
+    file1 = $formatter.file_path("file1.txt")
+    File.open(file1, 'w') {|f| f.puts "content for file1"}
+
+    file2 = $formatter.file_path("file2.txt", "Description for file2")
+    File.open(file2, 'w') {|f| f.puts "content for file2, with description"}
+    raise "spec fails!"
+  end
+
+  it "saves Watir 'Install' image" do
+    $browser.goto "http://www.watir.com"
+    image_path = $formatter.absolute_file_path("watir-install-logo.png")
+    $browser.image(:title => 'Install').save(image_path)
+    raise "spec fails again!"
+  end
+
   after :all do
     $browser.close
   end
